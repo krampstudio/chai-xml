b52967925faa63169a89374dabb5f2d7dac97c9e
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-09-07 16:47:04 +0200

    implement the XML comaprison

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
 chai-xml
 ========
 
-Xml assertions for Chai
+Xml assertions for Chai.
+
+## Install
+
+In addition to the [chai](http://chaijs.com) package, you need to install the plugin :
+
+```bash
+npm install chai-xml --save-dev
+```
+
+## Example
+
+```javascript
+var chai    = require('chai'); 
+var expect  = require('chai').expect;
+var chaiXml = require('chai-xml');
+
+//loads the plugin
+chai.use(chaiXml);
+
+describe('assert some xml', function(){
+
+    var someXml = '<root>\n\t<child name="foo" value="bar"></child>\n</root>';
+    var otherXml = '<root><child value="bar" name="foo" /></root>';
+
+    it("should be valid", function(){
+        expect(someXml).xml.to.be.valid();
+    });
+
+    it("should be the same string as otherXml ", function(){
+        expect(someXml).to.not.equal(otherXml);
+    });
+    it("should be the same XML as otherXml ", function(){
+        expect(someXml).xml.to.equal(otherXml);
+    });
+});
+```
+
+## Usage
+
+ - XML must be a *string*
+ - Add the property `xml` to your chain
+ - The `equal/eq/equals` methods compare XML instead of the strings
+ - The `valid` method check whether the XML is well-formed
+
+
+## Contributing
+
+Any contribution is welcome! Please check the [issues](https://github.com/krampstudio/chai-xml/issues). Do some unit tests as far as possible.
+ 
+## Release History
+ * _0.1.0_ initial release. Support `xml` property, `valid` method and overwrite the `equal/eq/equals` methods
+
+## License
+Copyright (c) 2014 Bertrand Chevrier  
+Licensed under the MIT license.
--- test/chai-xml-spec.js
+++ test/chai-xml-spec.js
@@ -1,16 +1,88 @@
+/**
+ * @author Bertrand Chevrier <chevrier.bertrand@gmail.com>
+ * @license MIT <https://raw.githubusercontent.com/krampstudio/chai-xml/master/LICENSE>
+ */
+
 var chai    = require('chai'); 
 var expect  = require('chai').expect;
 var chaiXml = require('../src/chai-xml');
 
+//loads the plugin
 chai.use(chaiXml);
 
-describe('XML assertions', function(){
+describe('chai-xml : ', function(){
 
-    describe('foo', function(){
-        it("should be a chai assertion method", function(){
+    describe('the xml member', function(){
+        it("should be a chai property", function(){
+            expect(chai.Assertion.prototype.__lookupGetter__('xml')).to.be.a('function');
+        });
+        it("should apply only on strings", function(){
+            expect('foo').xml.to.be.a('string');
+        
+            expect(function(){ 
+                expect({}).xml; 
+            }).to.throw();
 
-            expect(chai.Assertion.prototype.foo).to.be.a('function');
+            expect(function(){ 
+                expect(1).xml; 
+            }).to.throw();
         });
     }); 
 
+    
+    describe('the valid member', function(){
+        it("should be a chai method", function(){
+            expect(chai.Assertion.prototype.valid).to.be.a('function');
+        });
+        
+        it("should fail when object is invalid XML", function(){
+            [ '<root><child/>',
+              '<root><child>value</root></child>',
+              '<root>><</root>'
+            ].forEach(function(value){
+              expect(value).xml.not.valid();
+            });
+        });
+        it("should sucssed on valid XML", function(){
+            [ '<root><child/></root>',
+              '<root><child>value</child></root>',
+              '<root>&gt;&lt;</root>'
+            ].forEach(function(value){
+              expect(value).xml.valid();
+            });
+        });
+    });
+    
+    describe('the equal member', function(){
+        it("should be a chai method", function(){
+            expect(chai.Assertion.prototype.equal).to.be.a('function');
+        });
+        it("should not affect overriden beahvior", function(){
+            expect(true).to.equal(true);
+            expect(true).to.not.equal(false);
+            expect(null).to.equal(null);
+            expect('foo').to.equal('foo');
+            expect('foo').to.not.equal('bar');
+            expect(true).to.equal(true);
+        });
+        
+        it("should compare XML", function(){
+            expect('<root><child></child></root>').xml.to.equal('<root><child></child></root>');
+            expect('<root><child></child></root>').xml.to.equal('<root><child /></root>');
+            expect('<root><child></child></root>').xml.to.not.equal('<root><children /></root>');
+            expect('<root>\n\t<child name="foo" value="bar"></child>\n</root>').xml.to.equal('<root><child value="bar" name="foo" /></root>');
+        });
+    });
+    
+    describe('the equal aliases', function(){
+
+        it("should also compare XML", function(){
+            var actual = '<root>\n\t<child name="foo" value="bar"></child>\n</root>';
+            var expected = '<root><child value="bar" name="foo" /></root>';
+            expect(actual).xml.to.equal(expected);
+            expect(actual).xml.to.eq(expected);
+            expect(actual).xml.to.equals(expected);
+
+        });
+    });
 });
